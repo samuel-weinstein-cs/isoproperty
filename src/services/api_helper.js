3a433b654efd2e4d3dd616890186cbc5314300b1
sweinstein <raeweinst@gmail.com>
2020-04-10 16:46:33 -0400

    add image post in api helper

--- src/services/api_helper.js
+++ src/services/api_helper.js
@@ -9,24 +9,42 @@ export const getListings = async () => {
     return resp.data;
   }
 
-  export const getSingleListing = async (id) => {
-    const resp = await api.get(`/listings/${id}`)
-    return resp.data;
-  }
+export const getSingleListing = async (id) => {
+  const resp = await api.get(`/listings/${id}`)
+  return resp.data;
+}
 
-  export const loginUser = async (loginData) => {
-    const resp = await api.post('/auth/login', loginData);
-    console.log(resp);
-    api.defaults.headers.common.authorization = `Bearer ${resp.data.auth_token}`;
-    localStorage.setItem('authToken', resp.data.auth_token);
-    localStorage.setItem('name', resp.data.user.name);
-    localStorage.setItem('id', resp.data.user.id )
-    return resp.data.user;
-  }
+export const postImage = async (listingId, file) => {
+  const fileParts = file.name.split('.');
+  const signedResponse = await api.post(`/listings/${listingId}/image/`, {
+    fileName:fileParts[0],
+    fileType:fileParts[1]
+  })
+  console.log(signedResponse);
 
-  export const verifyUser = () => {
-    const token = localStorage.getItem('authToken');
-    if (token) {
-      api.defaults.headers.common.authorization = `Bearer ${token}`;
+  const options={
+    headers: {
+      'Content-Type': fileParts[1]
     }
   }
+  await axios.put(signedResponse.data.request, file, options);
+  console.log("File Uploaded");
+  return signedResponse;
+}
+
+export const loginUser = async (loginData) => {
+  const resp = await api.post('/auth/login', loginData);
+  console.log(resp);
+  api.defaults.headers.common.authorization = `Bearer ${resp.data.auth_token}`;
+  localStorage.setItem('authToken', resp.data.auth_token);
+  localStorage.setItem('name', resp.data.user.name);
+  localStorage.setItem('id', resp.data.user.id )
+  return resp.data.user;
+}
+
+export const verifyUser = () => {
+  const token = localStorage.getItem('authToken');
+  if (token) {
+    api.defaults.headers.common.authorization = `Bearer ${token}`;
+  }
+}
