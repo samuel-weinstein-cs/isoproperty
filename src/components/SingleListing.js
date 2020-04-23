e6ac18532f1ef339358e3c584b717beadbe566f2
samuel-weinstein-cs <raeweinst@gmail.com>
2020-04-23 17:16:04 -0400

    single listing changes

--- src/components/SingleListing.js
+++ src/components/SingleListing.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { getListings, getSingleListing } from "../services/api_helper";
+import { getSingleListing } from "../services/api_helper";
 
 class SingleListing extends Component {
   constructor(props) {
@@ -14,33 +14,9 @@ class SingleListing extends Component {
   }
 
   componentDidMount = async () => {
-    await getListings();
     const singleListing = await getSingleListing(this.props.id);
     this.setState({
       singleListing
-    });
-  };
-
-  setCurrentListing = () => {
-    const currentListing = this.props.listinghs.find(
-      listing => listing.id === parseInt(this.props.listingId)
-    );
-
-    this.setState({
-      currentListing
-    });
-  };
-
-  reset = () => {
-    this.setState({
-      currentListing: null
-    });
-  };
-
-  setListingId = () => {
-    const listingId = this.props.match.params.id;
-    this.setState({
-      listingId
     });
   };
 
@@ -50,16 +26,13 @@ class SingleListing extends Component {
         <h1>Property Details</h1>
         <h2>
           <b>
-            {this.state.singleListing.address}
-            {this.state.singleListing.address2} {this.state.singleListing.state}
-            {this.state.singleListing.city} {this.state.singleListing.zip}
+            {`${this.state.singleListing.address}${this.state.singleListing.address2}, ${this.state.singleListing.city}, ${this.state.singleListing.state} ${this.state.singleListing.zip}`}
           </b>
         </h2>
         <div className="single-listing-info">
         <p className="prop-details">
-          <span><b>Price:</b> {this.state.singleListing.price}</span>
-          <span><b>Rental:</b> {this.state.singleListing.rental}</span>
-          <span><b>Size:</b> {this.state.singleListing.size}</span>
+          <span><b>Price:</b> {`$${this.state.singleListing.price} ${this.state.singleListing.rental&&`per month`}`}</span>
+          <span><b>Size:</b> {`${this.state.singleListing.size} sq. ft`}</span>
           <span><b>Bedrooms:</b> {this.state.singleListing.bedrooms}</span>
           <span><b>Neighborhood:</b> {this.state.singleListing.neighborhood}</span>
           <span><b>Description:</b> {this.state.singleListing.description}</span>
