e8f85a9340a847a8936b9857368cbb64afd81057
samuel-weinstein-cs <raeweinst@gmail.com>
2020-05-08 16:38:36 -0400

    change some styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,13 +3,15 @@ import React from 'react';
 function Footer() {
     return(
         <div className="contact">
-        <p><i className="license"><b>Licensed Real Estate Broker</b></i><br></br>
-        <b>iso property corp</b><br></br>
-        New York, New York<br></br>
-        Phone 212.252.2704 (7days/week)<br></br>
-        <b className="email">contact@isoproperty.com</b><br></br></p>
-        <div className="upper-contact"></div>
+        <div className="upper-contact">
+          <p className="license">Licensed Real Estate Broker</p>
+          <p className="bold">iso property corp</p>
+          <p>New York, New York</p>
+          <p>Phone 212.252.2704 (7days/week)</p>
+          <a href="mailto:contact@isoproperty.com" className="email">bruh@bruh.bruh</a>
+        </div>
         <p>copyright© 2020 iso property corp All rights reserved</p>
+
         </div>
     )
 }
