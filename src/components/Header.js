53c9b91b89212e3ae505772b3f475679bf514c6e
samuel-weinstein-cs <raeweinst@gmail.com>
2020-05-08 17:05:50 -0400

    add image as link

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,11 +6,15 @@ import headerImage from "../images/iso_header_combined_vectorized.svg"
 function Header() {
   return (
     <div className="header">
-      <img
-        className="isoprop-logo"
-        src={headerImage}
-        alt="logo"
-      />
+      <Link to="/" style={{
+          outline:"none"
+        }}>
+        <img
+          className="isoprop-logo"
+          src={headerImage}
+          alt="logo"
+        />
+      </Link>
       <nav className="header-nav">
         <ul className="nav-list">
           <li className="header-link">
